d6b139011d801bf13694871b01c241d6b29942a5
Denis Shvetsov <dshvetsov@arccn.ru>
2017-10-26 11:20:42 +0300

    generator special sequance

---
+++ GenTriseq.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def GenTriseq(v):
+    for i in v:
+        less_than_i = filter(lambda x : i >= x, v)
+        for j in less_than_i:
+            yield j
+
+v = eval(input())
+n = eval(input())
+
+new_seq = list(GenTriseq(v))
+print(new_seq[n])
